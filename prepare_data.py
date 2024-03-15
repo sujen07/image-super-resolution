6a86f34722460c227388d01acfee65662201279e
BigSuj <sujenk@gmail.com>
2024-03-14 19:57:17 -0700

    add script to process images

--- prepare_data.py
+++ prepare_data.py
@@ -9,11 +9,9 @@ num_proc = 4  # Number of CPU Cores // 2
 out_dir = "data"  # Output directory for preprocessed images
 scale_factor = 4  # Scale factor for resizing, adjust based on your requirements
 
-# Load dataset
-dataset = load_dataset('eugenesiow/Div2k', 'bicubic_x4',  num_proc=num_proc)
 
 # Preprocess and save function
-def preprocess_and_save(data, subset):
+def preprocess_and_save(data, subset, start_ind=0):
     os.makedirs(f"{out_dir}/{subset}", exist_ok=True)  # Create output directory
     os.makedirs(f"{out_dir}/{subset}/lr", exist_ok=True)  # Low-res images directory
     os.makedirs(f"{out_dir}/{subset}/hr", exist_ok=True)  # High-res images directory
@@ -26,11 +24,15 @@ def preprocess_and_save(data, subset):
 
 
         # Save processed images
-        lr_img.save(f"{out_dir}/{subset}/lr/{i}.png")
-        hr_img.save(f"{out_dir}/{subset}/hr/{i}.png")
+        lr_img.save(f"{out_dir}/{subset}/lr/{i+start_ind}.png")
+        hr_img.save(f"{out_dir}/{subset}/hr/{i+start_ind}.png")
 
 # Process and save training and validation datasets
-preprocess_and_save(dataset['train'], 'train')
-preprocess_and_save(dataset['validation'], 'validation')
+# Load dataset
+if __name__ == '__main__':
+    dataset = load_dataset('eugenesiow/Div2k', 'bicubic_x4',  num_proc=num_proc)        
 
-print("Data preprocessing completed.")
+    preprocess_and_save(dataset['train'], 'train')
+    preprocess_and_save(dataset['validation'], 'validation')
+
+    print("Data preprocessing completed.")
---
+++ process_images.py
@@ -0,0 +1,33 @@
+import os
+import glob
+from PIL import Image
+
+from prepare_data import preprocess_and_save
+
+
+train_data = []
+img_folder = 'img_folder'
+
+all_imgs = glob.glob(os.path.join(img_folder, '*.png'))
+
+for image_path in all_imgs:
+    img = Image.open(image_path)
+
+    new_width = img.width // 4
+    new_height = img.height // 4
+
+    # Resize the image
+    img_resized = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+    # Path to save the downscaled image
+    downscaled_image_path = os.path.join(img_folder, os.path.basename(image_path))
+
+    # Save the downscaled image
+    img_resized.save(downscaled_image_path)
+
+    train_data.append({'lr': downscaled_image_path, 'hr': image_path})
+
+preprocess_and_save(train_data, 'train', start_ind=800)
+
+
+
